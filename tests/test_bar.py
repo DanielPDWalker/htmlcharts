d0bc52e5d91f69fe3f3b90d1f6562578471886a9
danielpdwalker <danielpdwalker@gmail.com>
2024-01-01 09:57:20 +0000

    Added bar_height setting tests for bar_chart function

--- tests/test_bar.py
+++ tests/test_bar.py
@@ -6,16 +6,36 @@ from pathlib import Path
 
 
 class TestBar(unittest.TestCase):
-    data = {"Mon": 4, "Tue": 2, "Wed": 8, "Thu": 1, "Fri": 0, "Sat": 3, "Sun": 2}
+    data = {"Mon": 4, "Tue": 2, "Wed": 10, "Thu": 1, "Fri": 0, "Sat": 3, "Sun": 2}
 
     def tearDown(self):
         if os.path.isfile(Path("html_chart.html")):
             os.remove(Path("html_chart.html"))
 
     def test_file_output_default(self):
-        bar_chart(self.data, file_name="html_chart")
+        bar_chart_html = bar_chart(self.data)
         self.assertFalse(os.path.isfile(Path("html_chart.html")))
+        self.assertTrue(bar_chart_html != None)
 
     def test_file_output_true(self):
-        bar_chart(self.data, file_output=True, file_name="html_chart")
+        bar_chart_html = bar_chart(self.data, file_output=True)
         self.assertTrue(os.path.isfile(Path("html_chart.html")))
+        self.assertTrue(bar_chart_html == None)
+
+    def test_bar_height_default_value(self):
+        bar_chart_html = bar_chart(self.data)
+        self.assertTrue('height="200"' in bar_chart_html)
+
+    def test_bar_height_custom_value(self):
+        custom_data = {
+            "Mon": 0,
+            "Tue": 0,
+            "Wed": 10,
+            "Thu": 0,
+            "Fri": 0,
+            "Sat": 0,
+            "Sun": 0,
+        }
+        bar_chart_html = bar_chart(custom_data, bar_height=500)
+        self.assertFalse('height="200"' in bar_chart_html)
+        self.assertTrue('height="500"' in bar_chart_html)
